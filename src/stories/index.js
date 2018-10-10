752396b02b6316bd33fe77af25c90be44ba93e4f
Chris Weber <christopher.m.weber@gmail.com>
2018-10-10 15:26:26 -0500

    Add Storybook.

---
+++ src/stories/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import {storiesOf} from '@storybook/react';
+import {action} from '@storybook/addon-actions';
+import {linkTo} from '@storybook/addon-links';
+
+import {Button, Welcome} from '@storybook/react/demo';
+import KaraokeForm from "../components/KaraokeForm";
+import SongList from "../components/SongList";
+import Song from "../components/Song";
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')}/>);
+
+storiesOf('Button', module)
+    .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+    .add('with some emoji', () => (
+        <Button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+        </Button>
+    ));
+
+storiesOf('KaraokeForm', module)
+    .add('standard form', () => <KaraokeForm/>);
+
+storiesOf('SongList', module)
+    .add('standard list', () => <SongList songs={[
+        {
+            id: 1528817077286,
+            singer: 'Chris Weber',
+            artist: 'Peter Gabriel',
+            song_name: 'In Your Eyes',
+            url: "",
+            completed: false
+        },
+        {
+            id: 1528817084358,
+            singer: 'Chris Weber',
+            artist: "Des'ree",
+            song_name: 'You Gotta Be',
+            url: "",
+            completed: false
+        }
+    ]}/>);
+
+storiesOf('Song', module)
+    .add('standard song', () => <Song
+        key={[1]}
+        song={[
+            {
+                id: 1,
+                singer: 'Chris Weber',
+                artist: "Des'ree",
+                song_name: 'You Gotta Be',
+                url: "https://www.youtube.com",
+                completed: false,
+            }
+        ]}
+    />);
